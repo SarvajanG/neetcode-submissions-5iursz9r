165a9b9557d7e477915e45efc05fecaf4ff75baa
SarvajanG <76636174+SarvajanG@users.noreply.github.com>
2026-06-12 01:45:01 -0700

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        cache = {}
+        n = len(nums) - 1
+        def memoization(i):
+           
+            if i > n:
+                return 0
+            elif i in cache:
+                return cache[i]
+            cache[i] = nums[i] + max(memoization(i + 2), memoization(i + 3))
+            return cache[i]
+        return max(memoization(0), memoization(1))
